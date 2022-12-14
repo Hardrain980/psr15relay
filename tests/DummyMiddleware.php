dcbc5b2317cb9d1d044e7798c404476c358f08f4
Leo <i@hardrain980.cm>
2022-12-14 13:52:26 +1100

    tests/DummyMiddleware.php, tests/DummyRequestHandler.php: create mocks for testing

---
+++ tests/DummyMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class DummyMiddleware implements MiddlewareInterface
+{
+	public function __construct(
+		private string $key,
+		private string $value
+	)
+	{
+
+	}
+
+	public function process(
+		ServerRequestInterface $request,
+		RequestHandlerInterface $handler
+	): ResponseInterface
+	{
+		return $handler->handle(
+			$request->withAddedHeader($this->key, $this->value)
+		);
+	}
+}
